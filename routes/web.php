2a87aa1a200644451f016430a63e9670930a3f62
chenyirou <annie851216@gmail.com>
2017-10-23 15:59:03 +0800

    練習六: 路由群組

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::get('student/{student_no}',function ($student_no){
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 }) -> where(['student_no' => 's[0-9]{10}','subject' => '(chinese|english|math)']);
-*/
+
 
 
 //練習五: 用Route的param方法替常用的參數統一限制，將學號統一進行格式設定
@@ -54,3 +54,16 @@ Route::get('student/{student_no}',function ($student_no){
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 }) -> where(['subject' => '(chinese|english|math)']);
+*/
+
+
+//練習六: 路由群組_透過prefix前綴，將網址前套上student
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix' => 'student'],function(){
+    Route::get('{student_no}', function ($student_no) {
+        return '學號：' . $student_no;
+    });
+    Route::get('{student_no}/score/{subject?}', function ($student_no, $subject = null) {
+        return '學號：' . $student_no . '的' . ((is_null($subject)) ? '所有科目' : $subject) . '成績';
+    })->where(['subject' => '(chinese|english|math)']);
+});
