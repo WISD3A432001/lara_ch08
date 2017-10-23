6eee1959a2484b5ec5ba799f07503e810d8443e3
chenyirou <annie851216@gmail.com>
2017-10-23 15:55:05 +0800

    練習五: 用Route的param方法，將學號統一進行格式設定

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('student/{student_no}/score/{subject}', function ($student_no,$subjec
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 });
-*/
+
 
 //練習四: 正規表達式限制參數
 Route::get('student/{student_no}',function ($student_no){
@@ -43,3 +43,14 @@ Route::get('student/{student_no}',function ($student_no){
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 }) -> where(['student_no' => 's[0-9]{10}','subject' => '(chinese|english|math)']);
+*/
+
+
+//練習五: 用Route的param方法替常用的參數統一限制，將學號統一進行格式設定
+Route::pattern('student_no','s[0-9]{10}');
+Route::get('student/{student_no}',function ($student_no){
+    return '學號：'.$student_no;
+});
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+    return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
+}) -> where(['subject' => '(chinese|english|math)']);
