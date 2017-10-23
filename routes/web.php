666b697586c6aa8d52b9c5f64d3ba53f1a2cdd41
chenyirou <annie851216@gmail.com>
2017-10-23 15:46:34 +0800

    練習三: 提供學生查詢所有科目或特定科目成績

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+/*
 //練習一: 顯示學生資料和成績
 Route::get('student/{student_no}',function ($student_no){
     return '學號：'.$student_no;
@@ -27,4 +27,10 @@ Route::get('student/{student_no}/score',function ($student_no){
 //練習二: 提供學生查詢自己的成績
 Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject){
     return '學號：'.$student_no.'的'.$subject.'成績';
+});
+*/
+
+//練習三: 提供學生查詢所有科目或特定科目的成績
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
+    return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 });
