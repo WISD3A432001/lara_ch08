ea39cd3252e98fed029a66541b3c977f41874420
chenyirou <annie851216@gmail.com>
2017-11-24 21:28:26 +0800

    撰寫TestTableSeeder->利用factory來產生假資料

--- database/seeds/TesrTableSeeder.php
+++ database/seeds/TesrTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Score as Score;
 class TesrTableSeeder extends Seeder
 {
     /**
@@ -11,6 +11,9 @@ class TesrTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $scores = factory(Score::class,20)->create()->each(function($score){
+            $score->total=$score->chinese+$score->english+$score->math;
+            $score->save();
+        });
     }
 }
