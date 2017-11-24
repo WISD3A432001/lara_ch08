b76b310942ead9e9aa36398be1b0f04a4edeb52e
chenyirou <annie851216@gmail.com>
2017-11-24 19:05:52 +0800

    在BoardController引用Score model

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Route;
 use View;
+use App\Score;
 class BoardController extends Controller
 {
     public function getIndex()
     {
-        return View::make('board');
+        return View('board');
     }
 }
