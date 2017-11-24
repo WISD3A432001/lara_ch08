5f9e609b2ffccb559fdab462320637fe365da28a
chenyirou <annie851216@gmail.com>
2017-11-24 20:01:20 +0800

    引用 Student model，修改 StudentController 之 getStudentData()方法及getStudentScore()方法

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,17 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use View;
+use App\Http\Requests;
+use App\Student;
 class StudentController extends Controller
 {
     public function getStudentData($student_no){
-        return View::make('score',[
-            'student_no'=>$student_no,
+        //return View::make('score',['student_no'=>$student_no, 'subject'=>null]);
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
             'subject'=>null
         ]);
     }
     public function getStudentScore ($student_no=null ,$subject = null){
-        return View::make('score',[
-            'student_no'=>$student_no,
+        //return View::make('score',['student_no'=>$student_no, 'subject'=>$subject]);
+        $student=Student::where('no',$student_no)->firstOrFail();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
             'subject'=>$subject
         ]);
     }
