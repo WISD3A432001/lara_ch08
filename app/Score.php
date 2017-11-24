e5a1fb660f66fd0eaeaf3b505f264d1679b3b788
chenyirou <annie851216@gmail.com>
2017-11-24 19:01:46 +0800

    在 Score 當中建立兩個 scope 方法

--- app/Score.php
+++ app/Score.php
@@ -13,4 +13,13 @@ class Score extends Model
     {
         return $this->belongsTo(StudentEloquent::class);
     }
+
+    //依總分排序
+    public function scopeOrderByTotal($query){
+        return $query->orderBy('score.total','DESC');
+    }
+    //依國英數排序
+    public function scopeOrderBySubject($query){
+        return $query->orderBy('score.chinese','DESC')->orderBy('score.english','DESC')->orderBy('score.math','DESC');
+    }
 }
